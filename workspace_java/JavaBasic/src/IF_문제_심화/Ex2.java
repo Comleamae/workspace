dd5e65d11163f98b6e3b3a9fc76f17b480d9af52
Comleamae <lbcy00@gmail.com>
2024-05-31 17:37:49 +0900

    1

--- workspace_java/JavaBasic/src/IF_문제_심화/Ex2.java
+++ workspace_java/JavaBasic/src/IF_문제_심화/Ex2.java
@@ -5,11 +5,18 @@ import java.util.Scanner;
 public class Ex2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("x 입력: ");
-        System.out.println("y 입력: ");
-        int x = sc.nextInt();
-        int y = sc.nextInt();
-        System.out.println("x = " + x);
-        System.out.println("y = " + y);
+        System.out.print("점 (x,y)의 좌표를 입력하시오>>");
+
+        int x = sc.nextInt(); int y = sc.nextInt();
+
+        if(x > 50 && x < 100){
+            if (y > 50 && y < 100) {
+                System.out.println("사각형 안에 점이 있습니다");
+            }
+        }
+        else {
+            System.out.println("없다");
+        }
+
     }
 }
--- workspace_java/JavaBasic/src/IF_문제_심화/Ex3.java
+++ workspace_java/JavaBasic/src/IF_문제_심화/Ex3.java
@@ -1,7 +1,14 @@
 package IF_문제_심화;
 
+import java.util.Scanner;
+
 public class Ex3 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("1~99 사이의 정수를 입력하시오>>");
+        int num = sc.nextInt();
+        60
 
+        }
     }
 }
