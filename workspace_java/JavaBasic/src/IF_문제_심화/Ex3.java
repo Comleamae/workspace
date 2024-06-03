a90154b3f920a819c1933eb2e03cb2f0bbf4a54a
Comleamae <lbcy00@gmail.com>
2024-06-03 11:47:43 +0900

    1

--- workspace_java/JavaBasic/src/IF_문제_심화/Ex3.java
+++ workspace_java/JavaBasic/src/IF_문제_심화/Ex3.java
@@ -7,8 +7,9 @@ public class Ex3 {
         Scanner sc = new Scanner(System.in);
         System.out.print("1~99 사이의 정수를 입력하시오>>");
         int num = sc.nextInt();
-        60
+        if(num/3 == 1 ){
 
+        }
         }
     }
 }
