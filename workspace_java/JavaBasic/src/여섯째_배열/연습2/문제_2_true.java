9a273dd8542280eacf1ec6053b9480aaa56e7d4a
Comleamae <lbcy00@gmail.com>
2024-06-05 17:39:41 +0900

    1

--- workspace_java/JavaBasic/src/여섯째_배열/연습2/문제_2_true.java
+++ workspace_java/JavaBasic/src/여섯째_배열/연습2/문제_2_true.java
@@ -3,13 +3,8 @@ package 여섯째_배열.연습2;
 public class 문제_2_true {
     public static void main(String[] args) {
         int[] arr = new int[100];
-        int prNum = 0;
-        int num = 0;
-        for(int i = 1; i < 100; i++){
-            arr[i] = i + 1;
-            if(arr[i] < 0){
-                System.out.println(arr[i]);
-            }
+        int[] prNum;
+        for(int e: prNum){
 
         }
     }
