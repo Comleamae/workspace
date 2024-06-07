04f945459bcee5212a0ec0b8006a1717300e4f58
Comleamae <lbcy00@gmail.com>
2024-06-07 16:22:07 +0900

    1

--- workspace_java/JavaBasic/src/두번째_변수와자료형/기본자료형의_형변환_0530.java
+++ workspace_java/JavaBasic/src/두번째_변수와자료형/기본자료형의_형변환_0530.java
@@ -8,7 +8,7 @@ public class 기본자료형의_형변환_0530 {
         double num2 = num1;
         System.out.println("num2 = " + num2);
 
-        //강제 형변환(Casting)
+        //강제 형변환(Casting) => 정수와 실수만 가능 아직은
         double num3 = 5.5;
         //int num4 = num3; 실수는 정수형에 저장불가 오류
         int num4 = (int)num3; //실수 데이터 num3를 int로 강제 형 변환
--- workspace_java/JavaBasic/src/여섯째_배열/연습2/문제_2_true.java
+++ workspace_java/JavaBasic/src/여섯째_배열/연습2/문제_2_true.java
@@ -1,11 +1,11 @@
-package 여섯째_배열.연습2;
-
-public class 문제_2_true {
-    public static void main(String[] args) {
-        int[] arr = new int[100];
-        int[] prNum;
-        for(int e: prNum){
-
-        }
-    }
-}
+//package 여섯째_배열.연습2;
+//
+//public class 문제_2_true {
+//    public static void main(String[] args) {
+//        int[] arr = new int[100];
+//        int[] prNum;
+//        for(int e: prNum){
+//
+//        }
+//    }
+//}
